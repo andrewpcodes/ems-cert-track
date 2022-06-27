d464abcc055c71d4f13ed6f570ec3af97e7af7a7
Andrew Peterson <apeterson247@gmail.com>
2022-06-27 14:13:17 -0400

    inital checklist page

--- src/pages/checklist/Checklist.tsx
+++ src/pages/checklist/Checklist.tsx
@@ -1,11 +1,48 @@
+import {
+  Box,
+  Chip,
+  Container,
+  CssBaseline,
+  Divider,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 function Checklist() {
   return (
-    <div>
-      <h1>THIS IS THE CHECKLIST PAGE</h1>
-      <p>Here is some test text</p>
-    </div>
+    <>
+      <Container component="main">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "left",
+          }}
+        >
+          <Typography component="h1" variant="h3">
+            Checklist
+          </Typography>
+          <Divider />
+          <Typography component="h1" variant="h6">
+            Airway/Respiration/Ventilation - <Chip label="Time: 1.5 hrs" />
+          </Typography>
+          <Typography component="h1" variant="h6">
+            Trauma - <Chip label="Time: 1.5 hrs" />
+          </Typography>
+          <Typography component="h1" variant="h6">
+            Medical - <Chip label="Time: 1.5 hrs" />
+          </Typography>
+          <Typography component="h1" variant="h6">
+            Operations - <Chip label="Time: 5 hrs" />
+          </Typography>
+          <Typography component="h1" variant="h6">
+            Cardiovascular - <Chip label="Time: 6 hrs" />
+          </Typography>
+        </Box>
+      </Container>
+    </>
   );
 }
 
