c0986d7896272e2521776edc2aab7a8edbd86456
Spathanasf1 <spathanasf@gmail.com>
2022-08-03 05:56:41 -0400

    <LocationSearch /> c&p directly from docs isn't working

--- src/pages/courses/Courses.tsx
+++ src/pages/courses/Courses.tsx
@@ -1,9 +1,35 @@
+import { Button } from "@mui/material";
 import React from "react";
 import Map from "./Map";
+import './courses.css';
+
+let course: string;
 
 function Courses() {
   return (
-    <Map />
+    <div className='coursePage'>
+      <div className='buttons'>
+        <Button style={{
+          position: 'absolute',
+          top: '40%',
+        }} onClick={() => {
+          course = 'BostonEMS';
+        }}>Boston EMS</Button>
+        <Button style={{
+          position: 'absolute',
+          top: '50%',
+        }} onClick={() => {
+          course = 'BostonUniversity'
+        }}>Boston University</Button>
+        <Button style={{
+          position: 'absolute',
+          top: '60%',
+          }} onClick={() => {
+            course = 'EMSAcademy'
+        }}>EMS Academy</Button>
+      </div>
+      <Map course={course} />
+    </div>
   );
 }
 
--- src/pages/courses/Map.tsx
+++ src/pages/courses/Map.tsx
@@ -1,16 +1,42 @@
-import { LocationSearch, MapView } from '@aws-amplify/ui-react';
+import { MapView, LocationSearch } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
-
 import '@aws-amplify/ui-react/styles.css';
-
 import awsExports from '../../aws-exports';
 
 Amplify.configure(awsExports);
 
-export default function Map() {
+const BostonEMS = {
+  course: 'BostonEMS',
+  latitude: 42.33443341004974,
+  longitude: -71.07411795231975,
+}
+
+const BostonUniversity = {
+  course: 'BostonUniversity',
+  latitude: 42.35231393693149,
+  longitude: -71.10543018318185
+}
+
+const EMSAcademy = {
+  course: 'EMSAcademy',
+  latitude: 42.25155326554675,
+  longitude: -71.00369200489789,
+}
+
+interface Props {
+  course: string
+};
+
+export default function Map(course: Props) {
   return (
-    <MapView>
-      <LocationSearch position='bottom-left' />
+    <MapView style={{
+      height: '500px',
+      width: '500px',
+      position: 'absolute',
+      left: '50%',
+      top: '12%',
+    }}>
+      <LocationSearch proximity={BostonEMS} />
     </MapView>
   );
 }
